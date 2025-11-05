3cd8a0124671b8ce7763f8053b9fad2e3017283e
duxn2901 <vdb2901k55@gmail.com>
2025-11-05 16:49:57 +0100

    Valid Parentheses

---
+++ src/leetcode/P20.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class P20 {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                top++;
+                stack[top] = s.charAt(i);
+            } 
+            if (s.charAt(i) == ')') {
+                if (top < 0 || stack[top] != '(') return false;
+                top--;
+            }
+            if (s.charAt(i) == ']') {
+                if (top < 0 || stack[top] != '[') return false;
+                top--;
+            }
+            if (s.charAt(i) == '}') {
+                if (top < 0 || stack[top] != '{') return false;
+                top--;
+            }
+        }
+        if (top >= 0) return false;
+        return true;
+    }
+}
