f8ae662f93a2d8d3f3f9456677ad597f5f2e52dc
duxn2901 <vdb2901k55@gmail.com>
2025-11-05 17:05:24 +0100

    Sqrt(x)

---
+++ src/leetcode/P69.py
@@ -0,0 +1,13 @@
+def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left, right = 2, x//2
+        while left <= right:
+            mid = (left + right) // 2
+            if mid ** 2 == x:
+                return mid
+            if mid ** 2 > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right
