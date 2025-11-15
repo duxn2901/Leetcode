8e3d7c92fedd415a6489c8abc4acf14ee62c4e42
duxn2901 <vdb2901k55@gmail.com>
2025-11-15 17:34:34 +0100

    Longest Consecutive Sequence

--- src/leetcode/P0128LongestConsecSeq.java
+++ src/leetcode/P0128LongestConsecSeq.java
@@ -1,5 +1,27 @@
 package leetcode;
+import java.util.HashSet;
 
 public class P0128LongestConsecSeq {
+    public int longestConsecutive(int[] nums) {
+        int result = 0;
+        HashSet<Integer> set = new HashSet<>();
 
+        for (int num: nums) {
+            set.add(num);
+        }
+        for (int i : set) {
+            if (!set.contains(i-1)) {
+                int count = 1;
+                int currentNum = i;
+                while (set.contains(currentNum+1)) {
+                    count++;
+                    currentNum++;
+                }
+                result = (count>result)? count : result;
+            }
+        }
+
+
+        return result;
+    }
 }
