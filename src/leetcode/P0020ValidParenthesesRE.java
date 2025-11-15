5b044d6500a4b2ba582fd8f64967b0b6d1a43629
duxn2901 <vdb2901k55@gmail.com>
2025-11-15 17:39:55 +0100

    Valid Parentheses using ArrayDeque

--- src/leetcode/P0020ValidParenthesesRE.java
+++ src/leetcode/P0020ValidParenthesesRE.java
@@ -1,5 +1,27 @@
 package leetcode;
 
+import java.util.ArrayDeque;
+
 public class P0020ValidParenthesesRE {
+    public boolean isValid(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') stack.push('(');
+            if (s.charAt(i) == '[') stack.push('[');
+            if (s.charAt(i) == '{') stack.push('{');
+            if (s.charAt(i) == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') return false;
+            }
+            if (s.charAt(i) == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') return false;
+            }
+            if (s.charAt(i) == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') return false;
+            }
+        }
+        if (!stack.isEmpty()) return false;
+        return true;
+    }
+    
 
 }
