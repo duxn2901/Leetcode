db913e07edd0a4998c1ada1a0a1f021501ae3bb8
duxn2901 <vdb2901k55@gmail.com>
2025-11-26 19:49:43 +0100

    Find Minimum Operations to Make All Elements Divisible by Three

--- src/leetcode/P3190MinOperationsToDiv3.java
+++ src/leetcode/P3190MinOperationsToDiv3.java
@@ -2,4 +2,11 @@ package leetcode;
 
 public class P3190MinOperationsToDiv3 {
 
+    public int minimumOperations(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num%3!=0) count++;
+        }
+        return count;
+    }
 }
