083ab55f261526d2845fa115ec136eb1f5394c7d
duxn2901 <vdb2901k55@gmail.com>
2025-11-26 20:41:24 +0100

    Greatest Sum Divisible by Three

---
+++ src/leetcode/P1262GreatestSumDiv3.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class P1262GreatestSumDiv3 {
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,2,3,4,4};
+        System.out.println(maxSumDivThree(nums));
+    }
+
+    public static int maxSumDivThree(int[] nums) {
+        int[] DP = new int[] {0, Integer.MIN_VALUE, Integer.MIN_VALUE}; // DP[0] = best sum %3 ==0, DP[1] = ... %3==1, [2]%3==2
+        int[] newDP = new int[3];
+        int modNow = 0;
+        for (int num : nums) {
+            newDP =  DP.clone();
+            for (int mod = 0; mod < 3; mod++) {
+                if (DP[mod] == Integer.MIN_VALUE) continue;
+                
+                modNow = (mod + num)%3;
+                int candidate = num + DP[mod];
+                if (candidate > newDP[modNow]) newDP[modNow] = candidate;
+                for (int n : newDP) {System.out.print(n + " ");} //TODO remove
+                System.out.println(); //TODO remove
+            }
+            DP = newDP.clone();
+        }
+
+
+        return DP[0];
+    }
+}
