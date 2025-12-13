4bcfe5c64a68585fb25e67634d72a78987470e68
duxn2901 <vdb2901k55@gmail.com>
2025-12-13 18:20:05 +0100

    Online Stock Span

--- src/leetcode/P0901OnlineStockSpan.java
+++ src/leetcode/P0901OnlineStockSpan.java
@@ -1,10 +1,30 @@
 package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class P0901OnlineStockSpan {
-    public StockSpanner() {
-        
+
+    public static void main(String[] args) {
+        P0901OnlineStockSpan obj = new P0901OnlineStockSpan();
+    }
+    
+    private Deque<int[]> stack;
+    public P0901OnlineStockSpan() {
+        stack = new ArrayDeque<>();
     }
     
     public int next(int price) {
-        
+        int count = 1;
+        while (true) {
+            if (stack.isEmpty() || price < stack.peek()[0]) {
+                stack.push(new int[] {price,count});
+                break;
+            }
+            if (price >= stack.peek()[0]) {
+                count+= stack.pop()[1];
+            }
+        }
+        return count;
     }
 }
