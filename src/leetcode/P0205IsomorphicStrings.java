096089e3c2b5f507142ef7e657aea9afe6c61ef1
duxn <vdb2901k55@gmail.com>
2026-01-05 17:11:05 +0100

    Isomorphic Strings, one-way hashmap check

--- src/leetcode/P0205IsomorphicStrings.java
+++ src/leetcode/P0205IsomorphicStrings.java
@@ -1,5 +1,23 @@
 package leetcode;
 
-public class P0205IsomorphicStrings {
+import java.util.HashMap;
 
+public class P0205IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+            if (!map.containsKey(charS)) {
+                if (map.values().contains(charT)) return false;
+                map.put(charS, charT);
+            } 
+            else {
+                if (map.get(charS) != charT) return false;
+            }
+        }
+        return true;
+    }
 }
