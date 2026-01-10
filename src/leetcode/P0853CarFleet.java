01750794cc2360aebd60e9f4edf522beb29d915a
duxn <vdb2901k55@gmail.com>
2026-01-10 19:06:03 +0100

    Car Fleet

--- src/leetcode/P0853CarFleet.java
+++ src/leetcode/P0853CarFleet.java
@@ -13,23 +13,20 @@ public class P0853CarFleet {
 
     public static int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
-        int fleetCount = 0;
+        int fleetCount = 1; //always count the first car as a fleet
         double[][] cars = new double[n][2]; // first param position, second time to reach target
         for (int i = 0; i < n; i++) {
             cars[i][0] = position[i];
             cars[i][1] = (double) (target - position[i]) / speed[i];
         }
         Arrays.sort(cars, (b,a) -> Double.compare(a[0], b[0])); //sort by first parameter(position)
-        double slowest = cars[0][1];
-
-        for (int car = 1; car < n; car++) {
+        double slowest = cars[0][1]; //first car
+        for (int car = 1; car < n; car++) { //iterate from second car on
             if (cars[car][1] > slowest) {
                 fleetCount++;
                 slowest = cars[car][1];
             }
-
         }
-
         return fleetCount;
     }
     
