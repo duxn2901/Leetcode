c9ca2a06986190ef451838df497085092fd3dae2
duxn <vdb2901k55@gmail.com>
2026-05-11 16:40:57 +0200

    Subsets II

---
+++ src/leetcode/P0090Subsets2.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class P0090Subsets2 {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,2,3};
+        System.out.println(subsetsWithDup(arr));
+    }
+
+    static List<List<Integer>> answer;
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        answer = new ArrayList<>();
+        int[] sortedArr = nums;
+        Arrays.sort(sortedArr);
+        helper(sortedArr, 0, new ArrayList<>());
+        return answer;
+    }
+
+    public static void helper(int[] nums, int index, List<Integer> current) {
+        if (index >= nums.length) {
+            answer.add(new ArrayList<>(current));
+            return;
+        }
+
+        if (index > 0 && !current.isEmpty() && nums[index] == current.get(current.size()-1)) {
+            ArrayList<Integer> newList = new ArrayList<>(current);
+            newList.add(nums[index]);
+            helper(nums, index+1, newList);
+            return;   
+        }
+
+        helper(nums, index+1, new ArrayList<>(current));
+        ArrayList<Integer> newList = new ArrayList<>(current);
+        newList.add(nums[index]);
+        helper(nums, index+1, newList);
+    }
+}
