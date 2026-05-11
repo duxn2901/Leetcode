0de5469c0c0b62c1b450d22435708c110fd34c45
duxn <vdb2901k55@gmail.com>
2026-05-11 15:49:02 +0200

    Subsets

---
+++ src/leetcode/P0078Subsets.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class P0078Subsets {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println(subsets(arr));
+    }
+
+    static List<List<Integer>> answer;
+    public static List<List<Integer>> subsets(int[] nums) {
+        answer = new ArrayList<>();
+        helper(nums, 0, new ArrayList<Integer>());
+        return answer;
+    }
+
+    public static void helper(int[] nums, int index, List<Integer> current) {
+        if (index >= nums.length) {
+            answer.add(new ArrayList<>(current));
+            return;
+        }
+        helper(nums, index+1, new ArrayList<>(current));
+        ArrayList<Integer> newList = new ArrayList<>(current);
+        newList.add(nums[index]);
+        helper(nums, index+1, newList);
+    }
+}
