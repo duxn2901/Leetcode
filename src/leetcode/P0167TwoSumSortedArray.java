0d4d7f921024dd9063885ce2961d259cada5c860
duxn <vdb2901k55@gmail.com>
2026-05-12 14:15:58 +0200

    Two Sum II - Input Array Is Sorted, unoptimal

---
+++ src/leetcode/P0167TwoSumSortedArray.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class P0167TwoSumSortedArray {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 1;
+        int right = numbers.length-1;
+        int targetInt = 0;
+        int[] answer = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            targetInt = target - numbers[i];
+            left = i+1;
+            right = numbers.length-1;
+            while (left <= right) {
+                int mid = (left + right) /2;
+                if (numbers[mid] == targetInt) {
+                    answer[0] = i+1;
+                    answer[1] = mid+1;
+                    return answer;
+                }
+                else if (numbers[mid] > targetInt) right = mid-1;
+                else left = mid+1;
+            }
+        }
+        return answer;
+    }
+}
