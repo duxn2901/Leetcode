d4d0076e0e1da9ff874e33171de2afeaf3bc5c95
duxn <vdb2901k55@gmail.com>
2026-05-18 18:48:40 +0200

    Repeated DNA Sequences. remove unused initial value

--- src/leetcode/P187.java
+++ src/leetcode/P187.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class P187 {
     public List<String> findRepeatedDnaSequences(String s) {
         int left = 0;
-        String current = "";
+        String current;
         ArrayList<String> list = new ArrayList<>();
         HashMap<String, Boolean> seen = new HashMap<>();
         if (s.length()<=10) return list;
