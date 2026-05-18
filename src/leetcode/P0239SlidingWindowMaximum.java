e1b609faf5bc36f8dbac6cd9c4d636f4ca71da87
duxn <vdb2901k55@gmail.com>
2026-05-18 19:25:02 +0200

    Sliding Window Maximum. unoptimized loop logic

---
+++ src/leetcode/P0239SlidingWindowMaximum.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+
+public class P0239SlidingWindowMaximum {
+    public static void main(String[] args) {
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        maxSlidingWindow(arr, 3);
+    }
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        int[] answer = new int[nums.length-k+1];
+        int left = 0;
+        int right = 0;
+        int i = 0;
+
+        while (right < k) {
+            while (!deque.isEmpty() && nums[right] > deque.getLast()) deque.pollLast();
+            deque.addLast(nums[right]);
+            right++;
+        }
+        while (right < nums.length) {
+            answer[i] = deque.getFirst();
+            i++;
+            if (!deque.isEmpty() && nums[left] == deque.getFirst()) deque.pollFirst();
+            left++;
+
+            while (!deque.isEmpty() && nums[right] > deque.getLast()) deque.pollLast();
+            deque.addLast(nums[right]);
+            right++;
+
+            
+        }
+        answer[i] = deque.getFirst();
+        
+        return answer;
+    }
+}
