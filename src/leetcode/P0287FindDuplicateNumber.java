989af279f334cc08b08a26676ed8a891058ffd00
duxn <vdb2901k55@gmail.com>
2026-05-29 00:42:52 +0200

    Find the Duplicate Number, solved using 2 slow & fast pointers

---
+++ src/leetcode/P0287FindDuplicateNumber.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class P0287FindDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do { 
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
