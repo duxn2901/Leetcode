6351b1630bed622ee124d0170326e468067c890f
duxn <vdb2901k55@gmail.com>
2026-06-05 21:58:16 +0200

    Total Waviness of Numbers in Range I, bruteforce

---
+++ src/leetcode/P3751TotalWaviness.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class P3751TotalWaviness {
+    public int totalWaviness(int num1, int num2) {
+        if (num2 <= 100) return 0;
+        int waviness = 0;
+        for (int i = num1; i <= num2; i++) {
+            if (i > 100) {
+                String s = i + "";
+                for (int j = 1; j < s.length()-1; j++) {
+                    if ((s.charAt(j) > s.charAt(j-1) && s.charAt(j) > s.charAt(j+1)) || (s.charAt(j) < s.charAt(j-1) && s.charAt(j) < s.charAt(j+1))) waviness++;
+                }
+            }
+        }
+        return waviness;
+    }
+}
